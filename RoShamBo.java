664f06bcf97b3e43501d5bf21c9134393d1fdffa
Nox Heidt <zoe.heidt@gmail.com>
2024-09-04 19:15:15 -0400

    making base classes and methods

--- RoShamBo.java
+++ RoShamBo.java
@@ -1,5 +1,20 @@
+
 public class RoShamBo {
     public static void main(String[]args){
+        rockBeatsScissorsTest();
+        paperBeatsRockTest();
+        scissorsBeatsPaperTest();
+    }
+    
+    public static void rockBeatsScissorsTest() {
+
+    }
+
+    public static void paperBeatsRockTest() {
+
+    }
+
+    public static void scissorsBeatsPaperTest() {
 
     }
 }
